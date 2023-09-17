d40f85c374dedb9a57d807f1a11cd1a28f7dd698
Boukind Rachid <boukind.r1@gmail.com>
2023-09-17 03:23:04 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * _printf: produces output according to a format
+ * _printf- produces output according to a format
  * @format: the format string
  * Return: The number of characters printed
 */
@@ -15,50 +15,35 @@ int _printf(const char *format, ...)
 	int i = 0;
 	char *str;
 
-	va_start (args, format);
+	va_start(args, format);
 
 	while (format && *format)
 	{
 		if (*format != '%')
-		{
-			write (1,format,1);
-			i++;
-		}
+			write(1, format, 1), i++;
 		else
 		{
 			format++;
 			if (*format == 'c')
 			{
 				char c = va_arg(args, int);
-				write (1, &c, 1);
-				i++;
-			}
 
+				write(1, &c, 1), i++;
+			}
 			else if (*format == 's')
 			{
 				str = va_arg(args, char *);
 				if (str)
 				{
 					while (*str)
-					{
-						write (1, str, 1);
-						str++;
-						i++;
-					}
+						write(1, str, 1), str++, i++;
 				}
-
 				else
 				{
-					write (1, ("NULL"), 6);
-					i += 6;
-				}
+					write(1, ("NULL"), 6), i += 6;
 			}
-
 			else if (*format == '%')
-			{
-				write (1, "%", 1);
-				i++;
-			}
+				write(1, "%", 1), i++;
 		}
 		format++;
 	}
